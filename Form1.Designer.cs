0b8cd3d28e08dac4673931ad68774c1b0ccd25cc
Aeron Ashbrook <aeronashbrook12@gmail.com>
2020-09-17 19:59:52 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Sum200Num
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHalfway = new System.Windows.Forms.TextBox();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Halfway";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sum";
+            // 
+            // txtHalfway
+            // 
+            this.txtHalfway.Location = new System.Drawing.Point(27, 104);
+            this.txtHalfway.Name = "txtHalfway";
+            this.txtHalfway.ReadOnly = true;
+            this.txtHalfway.Size = new System.Drawing.Size(100, 20);
+            this.txtHalfway.TabIndex = 2;
+            this.txtHalfway.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(27, 160);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.ReadOnly = true;
+            this.txtSum.Size = new System.Drawing.Size(100, 20);
+            this.txtSum.TabIndex = 3;
+            this.txtSum.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 334);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSum);
+            this.Controls.Add(this.txtHalfway);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHalfway;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sum200Num
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+
+            for (int i = 0; i <= 200; i++)
+            {
+                sum += i;
+                txtSum.Text = Convert.ToString(sum);
+
+                if (i == 100)
+                {
+                    txtHalfway.Text = Convert.ToString(sum);
+                }
+                Console.WriteLine("i = " + i + " \nsum = " + sum);
+            }
+        }
+    }
+}
